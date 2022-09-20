aa62b58eb8b67608d18b9fe50e85bd9203a1fbd2
brenley-woodard <brenleyew@gmail.com>
2022-09-20 11:36:02 -0700

    display of user changes corresponding to HP

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const fairyHP = document.getElementById('fairy-hp');
+const fairyImage = document.getElementById('fairy-image');
 /* State */
-
+let fairy = {
+    hp: 20,
+    type: 'fairy',
+};
 /* Events */
 
 /* Display Functions */
+function displayFairy() {
+    fairyHP.textContent = Math.max(0, fairy.hp);
+    if (fairy.hp < 1) {
+        fairyImage.src = 'assets/other/dead.png';
+    } else {
+        fairyImage.src = `assets/other/${fairy.type}.png`;
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+
+displayFairy();
