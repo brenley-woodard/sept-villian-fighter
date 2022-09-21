bb861f74eba651b40c4a38dfa58edf4eb4fd4f56
brenley-woodard <brenleyew@gmail.com>
2022-09-20 17:33:48 -0700

    villian image changing from state

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* Imports */
+import { renderVillian } from './render-utils.js';
 
 /* Get DOM Elements */
 // messages
@@ -7,15 +8,35 @@ const resultDisplay = document.getElementById('results-display');
 //fairy card
 const fairyHP = document.getElementById('fairy-hp');
 const fairyImage = document.getElementById('fairy-image');
+//villian card
+const villianList = document.getElementById('villian-list');
 
 /* State */
 let defeated = 0;
-let result = 'Click on villian to sprinkle with fairy dust...';
+let result = 'Click on a villian to hit them with glitter...';
 
 let fairy = {
     hp: 20,
     type: 'fairy',
 };
+
+let villians = [
+    {
+        name: 'Brody',
+        type: 'bat',
+        hp: 1,
+    },
+    {
+        name: 'Gerold',
+        type: 'ghost',
+        hp: 2,
+    },
+    {
+        name: 'Michelle',
+        type: 'mummy',
+        hp: 3,
+    },
+];
 
 /* Events */
 
@@ -37,8 +58,24 @@ function displayFairy() {
     }
 }
 
+function displayVillians() {
+    for (const villian of villians) {
+        const villianEl = renderVillian(villian);
+        villianList.append(villianEl);
+
+        /* villianEl.addEventListener('click', () => {
+            if (villian.hp < 1) {
+                result = `No use throwing glitter at a dead villian`;
+                displayResult();
+                return;
+            }
+        }); */
+    }
+}
+
 // (don't forget to call any display functions you want to run on page load!)
 
 displayFairy();
 displayResult();
 displayScoreboard();
+displayVillians();
